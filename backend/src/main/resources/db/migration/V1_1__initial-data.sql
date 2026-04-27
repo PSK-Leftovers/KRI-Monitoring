07ef2fca19988bea92dd444a96cf1789db366fa4
eimanteras <sostakas18@gmail.com>
2026-04-27 13:40:10 +0300

    KRI-2 Add indicator database schema

--- backend/src/main/resources/db/migration/V1_1__initial-data.sql
+++ backend/src/main/resources/db/migration/V1_1__initial-data.sql
@@ -7,3 +7,13 @@ INSERT INTO books (title, author, published_year)
 VALUES ('Clean Code', 'Robert C. Martin', 2008),
        ('The Pragmatic Programmer', 'Andrew Hunt', 1999),
        ('Designing Data-Intensive Applications', 'Martin Kleppmann', 2017);
+INSERT INTO rodiklis (pavadinimas, aprasymas, riba_zalia, riba_geltona, riba_raudona)
+VALUES
+    ('Likvidumo rodiklis', 'Stebi organizacijos likvidumo riziką', 1.5, 1.0, 0.5),
+    ('Skolos rodiklis', 'Stebi organizacijos skolos lygį', 30.0, 50.0, 70.0);
+
+INSERT INTO books (title, author, published_year)
+VALUES
+    ('Clean Code', 'Robert C. Martin', 2008),
+    ('The Pragmatic Programmer', 'Andrew Hunt', 1999),
+    ('Designing Data-Intensive Applications', 'Martin Kleppmann', 2017);
