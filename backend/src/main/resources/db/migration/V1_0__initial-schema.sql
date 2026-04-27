357b6bcb5d0aadf06bdbae7b273aa93348b08a1a
eimanteras <sostakas18@gmail.com>
2026-04-27 13:40:10 +0300

    KRI-2 Add indicator database schema

--- backend/src/main/resources/db/migration/V1_0__initial-schema.sql
+++ backend/src/main/resources/db/migration/V1_0__initial-schema.sql
@@ -1,3 +1,13 @@
+CREATE TABLE rodiklis
+(
+    id            BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    pavadinimas   VARCHAR(255) NOT NULL,
+    aprasymas     TEXT,
+    riba_zalia    DOUBLE PRECISION,
+    riba_geltona  DOUBLE PRECISION,
+    riba_raudona  DOUBLE PRECISION
+);
+
 CREATE TABLE books
 (
     id             BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
