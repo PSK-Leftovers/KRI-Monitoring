0dd4950bf0b01943adfe9e66a6f87b4f1e56b165
Tomas <128937653+martinkadev@users.noreply.github.com>
2026-05-11 00:39:08 +0300

    Refactor email service into interface with SMTP implementation

--- backend/src/main/java/com/leftovers/kri/notification/EmailService.java
+++ backend/src/main/java/com/leftovers/kri/notification/EmailService.java
@@ -1,35 +1,5 @@
 package com.leftovers.kri.notification;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
-
-@Slf4j
-@Service
-public class EmailService {
-    private final JavaMailSender mailSender;
-    private final String fromAddress;
-
-    public EmailService(
-            JavaMailSender mailSender,
-            @Value("${app.email.from}") String fromAddress
-    ) {
-        this.mailSender = mailSender;
-        this.fromAddress = fromAddress;
-    }
-
-    public void sendEmail(String to, String subject, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-
-        message.setFrom(fromAddress);
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
-
-        mailSender.send(message);
-
-        log.info("Email sent to={} with subject={}", to, subject);
-    }
+public interface EmailService {
+    void sendEmail(String to, String subject, String body);
 }
