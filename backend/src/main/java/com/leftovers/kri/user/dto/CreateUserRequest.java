ac35539bf9e206c00936f2248ff07405500fe7ac
MrMnt <73601367+MrMnt@users.noreply.github.com>
2026-05-11 11:13:14 +0300

    Replace with single class imports

--- backend/src/main/java/com/leftovers/kri/user/dto/CreateUserRequest.java
+++ backend/src/main/java/com/leftovers/kri/user/dto/CreateUserRequest.java
@@ -1,6 +1,10 @@
 package com.leftovers.kri.user.dto;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record CreateUserRequest(
         @NotBlank
