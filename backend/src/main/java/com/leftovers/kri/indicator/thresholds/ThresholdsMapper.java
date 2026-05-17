672274453ed68556fe17b8adfdf775c0605e5fa3
ZygimantasVidmantas <zygimantas.vidmantas@mif.stud.vu.lt>
2026-05-18 00:46:29 +0300

    Fixed null error when mapping to thresholds

--- backend/src/main/java/com/leftovers/kri/indicator/thresholds/ThresholdsMapper.java
+++ backend/src/main/java/com/leftovers/kri/indicator/thresholds/ThresholdsMapper.java
@@ -13,7 +13,6 @@ import com.leftovers.kri.indicator.thresholds.dto.ThresholdsResponse;
 @Mapper(componentModel = "spring")
 public interface ThresholdsMapper {
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "indicator", ignore = true)
     @Mapping(target = "recordedAt", ignore = true)
     Thresholds toEntity(CreateIndicatorRequest request, Indicator indicator);
 
