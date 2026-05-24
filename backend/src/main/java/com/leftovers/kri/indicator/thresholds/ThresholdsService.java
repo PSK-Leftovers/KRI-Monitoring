e7607cf467bd394efe3c1f587b1b2a98c032cda9
ZygimantasVidmantas <zygimantas.vidmantas@mif.stud.vu.lt>
2026-05-24 18:24:15 +0300

    Implemented thresholds history to show firstBeforeAfter for graphing

--- backend/src/main/java/com/leftovers/kri/indicator/thresholds/ThresholdsService.java
+++ backend/src/main/java/com/leftovers/kri/indicator/thresholds/ThresholdsService.java
@@ -10,9 +10,12 @@ import lombok.RequiredArgsConstructor;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.PredicateSpecification;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,17 +25,29 @@ public class ThresholdsService {
     private final ThresholdsMapper thresholdsMapper;
 
     public ThresholdsResponse getThresholdChangesByIndicatorId(Long indicatorId, @Nullable Instant after, @Nullable Instant before) {
-        PredicateSpecification<Thresholds> query = ThresholdsSpecifications.hasIndicatorWithId(indicatorId);
+        PredicateSpecification<Thresholds> criteria = ThresholdsSpecifications.hasIndicatorWithId(indicatorId);
 
         if (after != null) {
-            query = query.and(ThresholdsSpecifications.recordedAfter(after));
+            Optional<Thresholds> firstBeforeAfter = thresholdsRepository.findBy(
+                ThresholdsSpecifications.hasIndicatorWithId(indicatorId)
+                .and(ThresholdsSpecifications.recordedBefore(after)),
+                query -> query.sortBy(Sort.by(Sort.Direction.DESC, "recordedAt")).first());
+
+            if (firstBeforeAfter.isPresent()) {
+                criteria = criteria.and(ThresholdsSpecifications.recordedAfter(firstBeforeAfter.get().getRecordedAt()));
+            } else {
+                criteria = criteria.and(ThresholdsSpecifications.recordedAfter(after));
+            }
         }
 
         if (before != null) {
-            query = query.and(ThresholdsSpecifications.recordedBefore(before));
+            criteria = criteria.and(ThresholdsSpecifications.recordedBefore(before));
         }
         
-        List<Thresholds> history = thresholdsRepository.findAll(query);
+        List<Thresholds> history = thresholdsRepository.findAll(Specification.where(criteria), Sort.by(Sort.Direction.DESC, "recordedAt"));
+        
+        if (after != null) {
+        }
 
         List<ThresholdChange> greenChanges =
             extractChanges(history, Thresholds::getGreenThreshold);
