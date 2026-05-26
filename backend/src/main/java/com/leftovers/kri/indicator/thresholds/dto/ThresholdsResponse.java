9602c9fadb4dccc039d687e6d85b4be60ca2b057
MrMnt <73601367+MrMnt@users.noreply.github.com>
2026-05-26 19:31:24 +0300

    Fix build error
    ThresholdsServiceTest.java:[55,32] cannot find symbol
    [ERROR]   symbol:   method builder()

--- backend/src/main/java/com/leftovers/kri/indicator/thresholds/dto/ThresholdsResponse.java
+++ backend/src/main/java/com/leftovers/kri/indicator/thresholds/dto/ThresholdsResponse.java
@@ -1,7 +1,10 @@
 package com.leftovers.kri.indicator.thresholds.dto;
 
+import lombok.Builder;
+
 import java.util.List;
 
+@Builder
 public record ThresholdsResponse(
     List<ThresholdChange> green,
     List<ThresholdChange> yellow,
